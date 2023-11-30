c960d5f282a3ceb4d12a51d880335225c6ffebc5
Dhanuka Dulanjana <DhanukaD@labfriend.com>
2023-11-30 21:16:33 +0530

    intializing project

---
+++ cypress.config.js
@@ -0,0 +1,62 @@
+const { defineConfig } = require("cypress");
+
+const sqlServer = require("cypress-sql-server");
+
+const dbSettings = {
+  userName: "sa",
+  password: "EastTower56#$",
+  server: "lf-test-db.southeastasia.cloudapp.azure.com",
+  options: {
+    database: "JMDB_dev",
+    encrypt: true,
+    rowCollectionOnRequestCompletion: true,
+    trusted_connection: true,
+    multipleActiveResultSets: true,
+    trustServerCertificate: true,
+  },
+};
+
+module.exports = defineConfig({
+  projectId: "h68k6i",
+  projectId: "iauyf5",
+  e2e: {
+    watchForFileChanges: true,
+    video: true,
+    videoCompression: 32,
+    reporter: "mochawesome",
+    reporterOptions: {
+      charts: true,
+      overwrite: false,
+      html: false,
+      json: true,
+      reportDir: "cypress/reports",
+    },
+    setupNodeEvents(on, config) {
+      // config.db = {
+      //   "userName": "sa",
+      //   "password": "EastTower56#$",
+      //   "server": "lf-test-db.southeastasia.cloudapp.azure.com",
+      //   "options": {
+      //     "database": "JMDB_dev",
+      //     "encrypt": true,
+      //     "rowCollectionOnRequestCompletion": true
+      //   }
+      // }
+
+      // code from /plugins/index.js
+
+      const tasks = sqlServer.loadDBPlugin(dbSettings);
+      //const tasks = sqlServer.loadDBPlugin(config.db);
+      on("task", tasks);
+
+      return config;
+      // allows db data to be accessed in tests
+      //  const tasks = sqlServer.loadDBPlugin(dbSettings);
+      //  on('task', tasks);
+
+      //  return config;
+    },
+  },
+  video: true,
+  videoCompression: 32,
+});
---
+++ cypress/e2e/pages/Test.cy.js
@@ -0,0 +1,45 @@
+
+import testDataFile from '../../fixtures/testData/test.json'
+
+
+describe.skip('template spec', () => {
+   
+  
+
+  let testdata;
+
+  before(()=>{
+    cy.fixture("testData\\test.json").then((data)=>{
+      testdata =data;
+    })
+    
+  })
+  
+  
+
+  it.only('passes', () => {
+      cy.viewport(1300,660);
+      cy.visit('https://au.jm-dev.store/');
+      let obj = JSON.parse(JSON.stringify(testDataFile));
+      cy.log(obj.Username);
+
+      cy.sqlServer("SELECT * FROM dbo.ProductReview").then((users) =>{
+        cy.log(users);
+        let new_Array =users;
+        cy.log(new_Array[0][0]);
+
+    });
+
+ 
+  })
+
+  it('Search_Sku', ()=>{
+    cy.viewport(1300,660)
+    cy.visit('https://au.jm-dev.store/');
+    cy.wait(3000);
+    cy.log("welcome");
+    cy.contains('Search').click();
+    cy.xpath("//input[@placeholder='Search John Morris...']").type("vghvdd");
+
+  })
+})
